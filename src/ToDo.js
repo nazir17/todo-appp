85340850f9a15ef307feb0372c2b1b9a4b2cfd98
nazir17 <naziruddin@tothebyte.com>
2025-08-20 13:15:05 +0530

    now user can add todo

---
+++ src/ToDo.js
@@ -0,0 +1,62 @@
+import { Component } from "react";
+
+class ToDo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ newTodo: event.target.value });
+  };
+
+  handleAdd = (event) => {
+    event.preventDefault();
+    const text = this.state.newTodo.trim();
+    if (!text) {
+      return;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text,
+    };
+
+    this.setState((prev) => ({
+      todos: [...prev.todos, newTodo],
+      newTodo: "",
+    }));
+  };
+  
+  render() {
+    const { todos, newTodo } = this.state;
+    return (
+      <div>
+        <h1>Todo App</h1>
+        <form onSubmit={this.handleAdd}>
+          <input
+            type="text"
+            placeholder="Add Your Todo..."
+            value={newTodo}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+        <div>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                {todo.text}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ToDo;
