16bdb7b9f0ca821b37bbdcc6c9f85da15c3eec64
nazir17 <naziruddin@tothebyte.com>
2025-08-20 16:53:35 +0530

    added error message for empty todo

---
+++ src/ToDo.js
@@ -0,0 +1,85 @@
+import { Component } from "react";
+import "./ToDo.css";
+
+class ToDo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: "",
+      error: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ newTodo: event.target.value });
+  };
+
+  handleAdd = (event) => {
+    event.preventDefault();
+    const text = this.state.newTodo.trim();
+    if (!text) {
+      this.setState({ error: "Todo cannot be empty!" });
+      return;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+
+    this.setState((prev) => ({
+      todos: [...prev.todos, newTodo],
+      newTodo: "",
+      error: "",
+    }));
+  };
+
+  handleToggle = (id) => {
+    this.setState((prev) => ({
+      todos: prev.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    }));
+  };
+
+  render() {
+    const { todos, newTodo, error } = this.state;
+    return (
+      <div>
+        <h1>Todo App</h1>
+        <form onSubmit={this.handleAdd}>
+          <input
+            type="text"
+            className={`${error ? "input-error" : ""}`}
+            placeholder="Add Your Todo..."
+            value={newTodo}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {error}
+        <div className="div">
+          <ul>
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className={`todo-item ${todo.completed ? "completed" : ""}`}
+              >
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => this.handleToggle(todo.id)}
+                />
+                <span>{todo.text}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ToDo;
